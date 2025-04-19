8975b026d3ac967d64b576836dd0c9b2bd1f99a7
fufex3934 <fufawakgari174@gmail.com>
2025-04-19 09:44:48 +0300

    Nextjs NotesApp

---
+++ src/models/Notes.tsx
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const notesSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.models.Notes || mongoose.model("Note", notesSchema);
