b7a486f7c535094f56f8d48dd3895682e9c32b5d
fufex3934 <fufawakgari174@gmail.com>
2025-04-19 10:20:21 +0300

    finishing create note

--- src/app/create-note/page.tsx
+++ src/app/create-note/page.tsx
@@ -1,21 +1,25 @@
-import Link from "next/link"
+import Link from "next/link";
+import { createNote } from "../lib/actions";
 const CreateNote = () => {
   return (
-    <div>
-        <form action="">
-            <label htmlFor="title">Title</label>
-            <input type="text" name="title" id="title" placeholder='Title' />
+    <>
+      <form action={createNote} className="max-w-xl mx-auto p-4 space-y-4">
+        <h1 className="text-2xl font-bold">New Note</h1>
 
-            <label htmlFor="description">Description</label>
-            <textarea rows={5} name='description' id='description' placeholder='Note Content' />
-            <div>
-            <button type='submit'>Save</button>
-            <Link href={'/'}>Cancel</Link>
+        <input 
+        className="w-full border p-2 rounded"
+        type="text" name="title" placeholder="Title" />
 
-            </div>
-        </form>
-    </div>
-  )
-}
+        <textarea
+         className="w-full border p-2 rounded"
+         rows={5} name="description" placeholder="Note Content" />
+        <div>
+          <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">Save</button>
+          <Link href={"/"} className="bg-red-500 text-white px-4 py-3 ml-1 rounded">Cancel</Link>
+        </div>
+      </form>
+    </>
+  );
+};
 
-export default CreateNote
+export default CreateNote;
---
+++ src/app/lib/actions.ts
@@ -0,0 +1,18 @@
+'use server'
+
+import { connectToDB } from "@/dbConfig/db";
+import Note from "@/models/Note";
+import { revalidatePath } from "next/cache";
+export async function createNote(formData:FormData) {
+    const title = formData.get('title') as string;
+    const description = formData.get('description') as string;
+
+    if(!title || !description){
+        throw new Error("Missing fields")
+    }
+    
+    await connectToDB();
+    await Note.create({title,description});
+
+    revalidatePath('/notes');
+}
